b502100146ffc095928a1f06622b7c24ec159718
Vang Huynh <huynh.vang@devexperts.com>
2024-10-25 08:35:03 +0700

    disable corse

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.useGlobalPipes(new ValidationPipe());
+	app.enableCors();
 
 	const config = new DocumentBuilder()
 		.setTitle('ASM3')
@@ -19,3 +20,4 @@ async function bootstrap() {
 	await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
+
